a9c2ae796ef3a42c3e403a60da703427a9be07f3
Hayden Farnsworth <hfmets@gmail.com>
2022-02-01 12:30:00 -0600

    change cors origin

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: false }));
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "vandelay-user.fun",
+  origin: "*",
   credentials: true,
 };
 app.use(cors(corsOptions));
